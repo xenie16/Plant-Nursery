9be81174997764b7cfec5f12b812fe53eed13345
Haike <haike16@hotmail.com>
2024-12-20 00:11:31 +0100

    Added products are visible in cart + price is updated according to quantity + product is deleted if amount is 0 + total cost is shown

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,68 @@
 import Navbar from "../components/Navbar";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  removeFromCart,
+  updateQuantity,
+  selectCartItems,
+} from "../redux/slices/cartSlice";
 
 function Cart() {
+  const cartItems = useSelector(selectCartItems);
+  const dispatch = useDispatch();
+
+  const totalPrice = cartItems
+    .reduce((total, item) => total + item.product.price * item.quantity, 0)
+    .toFixed(2);
+
+  const handleQuantityChange = (id, newQuantity) => {
+      dispatch(updateQuantity({ id, quantity: newQuantity }));
+
+    if (newQuantity <= 0) {
+      dispatch(removeFromCart({ id }));
+    }
+  };
   return (
     <>
       <Navbar />
       <h1>Cart</h1>
+      <p>Total Cost: € {totalPrice}</p>
+      {cartItems.length === 0 ? (
+        <p>Your cart is empty</p>
+      ) : (
+        cartItems.map((item) => (
+          <div key={item.id} className="cart-item">
+            <img
+              src={item.src}
+              alt={item.alt}
+              className="cart-item__image"
+            ></img>
+            <h2>{item.name}</h2>
+            <p>Price: € {item.product.price}</p>
+            <div className="cart-item__quantity">
+              <button
+                onClick={() =>
+                  handleQuantityChange(item.product.id, item.quantity - 1)
+                }
+                type="button"
+              >
+                -
+              </button>
+              <p>{item.quantity}</p>
+              <button
+                onClick={() =>
+                  handleQuantityChange(item.product.id, item.quantity + 1)
+                }
+                type="button"
+              >
+                +
+              </button>
+            </div>
+            <p>
+              Total Cost: € {(item.product.price * item.quantity).toFixed(2)}
+            </p>
+          </div>
+        ))
+      )}
     </>
   );
 }
--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -10,13 +10,45 @@ const cartSlice = createSlice({
    initialState,
    reducers: {
       addToCart: (state, action) => {
-         state.items.push(action.payload);
+         const existingItem = state.items.find(item => item.id === action.payload.id);
+
+         if (existingItem) {
+            existingItem.quantity += 1;
+         } else {
+            state.items.push({
+               product: action.payload,
+               quantity: 1,
+            })
+
+         }
          state.totalItems += 1;
       },
+      removeFromCart: (state, action) => {
+         const existingItem = state.items.find(item => item.product.id === action.payload.id);
+
+         if (existingItem) {
+            state.items = state.items.filter(item => item.id !== action.payload.id);
+            state.totalItems -= 1;
+         }
+      },
+      updateQuantity: (state, action) => {
+         const { id, quantity } = action.payload;
+         const item = state.items.find(item => item.product.id === id);
+
+         if (item) {
+            const difference = quantity - item.quantity;
+            item.quantity = quantity;
+            state.totalItems += difference;
+         }
+
+         if (quantity === 0) {
+            state.items = state.items.filter(item => item.product.id !== id);
+         }
+      }
    }
 })
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, updateQuantity } = cartSlice.actions;
 export const selectCartItems = (state) => state.cart.items;
 export const selectTotalItems = (state) => state.cart.totalItems;
 export default cartSlice.reducer;
