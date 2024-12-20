7292d4998286df78263110092ca802f0a16f3892
Haike <haike16@hotmail.com>
2024-12-20 02:13:51 +0100

    Ensured total price and checkout button only show if items are in cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -43,68 +43,78 @@ function Cart() {
       <Navbar />
       <section className="cart">
         <h1>Cart</h1>
-        <p>Total Cost: € {totalPrice}</p>
         {cartItems.length === 0 ? (
           <p>Your cart is empty</p>
         ) : (
-          <div className="cart-items">
-            {cartItems.map((item) => (
-              <div key={item.product.id} className="cart-items__product">
-                <img
-                  src={item.product.src}
-                  alt={item.product.alt}
-                  className="cart-items__product-image"
-                ></img>
-                <div className="cart-items__product-details">
-                  <h2 className="cart-items__product-name">
-                    {item.product.name}
-                  </h2>
-                  <p className="cart-items__product-price">
-                    Price: € {item.product.price}
-                  </p>
-                  <div className="cart-items__product-quantitycontrol">
-                    <button
-                      className="cart-items__product-removeButton"
-                      onClick={() =>
-                        handleQuantityChange(item.product.id, item.quantity - 1)
-                      }
-                      type="button"
-                    >
-                      -
-                    </button>
-                    <p className="cart-items__product-quantity">
-                      {item.quantity}
+          <>
+            <p>Total Cost: € {totalPrice}</p>
+            <div className="cart-items">
+              {cartItems.map((item) => (
+                <div key={item.product.id} className="cart-items__product">
+                  <img
+                    src={item.product.src}
+                    alt={item.product.alt}
+                    className="cart-items__product-image"
+                  ></img>
+                  <div className="cart-items__product-details">
+                    <h2 className="cart-items__product-name">
+                      {item.product.name}
+                    </h2>
+                    <p className="cart-items__product-price">
+                      Price: € {item.product.price}
+                    </p>
+                    <div className="cart-items__product-quantitycontrol">
+                      <button
+                        className="cart-items__product-removeButton"
+                        onClick={() =>
+                          handleQuantityChange(
+                            item.product.id,
+                            item.quantity - 1
+                          )
+                        }
+                        type="button"
+                      >
+                        -
+                      </button>
+                      <p className="cart-items__product-quantity">
+                        {item.quantity}
+                      </p>
+                      <button
+                        className="cart-items__product-addButton"
+                        onClick={() =>
+                          handleQuantityChange(
+                            item.product.id,
+                            item.quantity + 1
+                          )
+                        }
+                        type="button"
+                      >
+                        +
+                      </button>
+                    </div>
+                    <p className="cart-items__product-totalCost">
+                      Total Cost: €{" "}
+                      {(item.product.price * item.quantity).toFixed(2)}
                     </p>
                     <button
-                      className="cart-items__product-addButton"
-                      onClick={() =>
-                        handleQuantityChange(item.product.id, item.quantity + 1)
-                      }
+                      className="cart-items__product-deleteButton"
+                      onClick={() => handleRemoveItem(item.product.id)}
                       type="button"
                     >
-                      +
+                      Delete
                     </button>
                   </div>
-                  <p className="cart-items__product-totalCost">
-                    Total Cost: €{" "}
-                    {(item.product.price * item.quantity).toFixed(2)}
-                  </p>
-                  <button
-                    className="cart-items__product-deleteButton"
-                    onClick={() => handleRemoveItem(item.product.id)}
-                    type="button"
-                  >
-                    Delete
-                  </button>
                 </div>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
+          </>
         )}
         <div className="cart__buttons">
-          <button onClick={checkoutAlert} type="button">
-            Checkout
-          </button>
+          {cartItems.length !== 0 && (
+            <button onClick={checkoutAlert} type="button">
+              Checkout
+            </button>
+          )}
           <button onClick={productPageRedirect} type="button">
             Continue Shopping
           </button>
